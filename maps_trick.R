31fa3aadb4a322a84c187862263eed55cf2ceca9
Dave Michelson <daveism@gmail.com>
2017-10-02 10:21:43 -0400

    update the map to replicate red n blue maps

--- maps_trick.R
+++ maps_trick.R
@@ -3,10 +3,10 @@
 #############################
 #personal
 greater40 <- subset(counties_yale_map, as.numeric(counties_yale_map$personal) >= 40)
-greater40$trickmap1value = 100
+greater40$trickmap1value = 0
 
 less40 <- subset(counties_yale_map, as.numeric(counties_yale_map$personal) < 40)
-less40$trickmap1value = 0
+less40$trickmap1value = 100
 
 trickdata1 <- rbind(less40,greater40)
 counties$trickmap1value <- trickdata1$trickmap1value[match(counties$yale_match, trickdata1$yale_match)]
@@ -16,10 +16,10 @@ ggsave(trick_map_1, file = paste(output_maps,"trick_map_1.png"), width = 6, heig
 
 #per_gop
 greater50 <- subset(yale_election_merge, as.numeric(yale_election_merge$per_gop) >= 50)
-greater50$trickmap2value = 0
+greater50$trickmap2value = 100
 
 less50 <- subset(yale_election_merge, as.numeric(yale_election_merge$per_gop) < 50)
-less50$trickmap2value = 100
+less50$trickmap2value = 0
 
 trickdata2 <- rbind(less50,greater50)
 counties$trickmap2value <- trickdata2$trickmap2value[match(counties$yale_match, trickdata2$yale_match)]
